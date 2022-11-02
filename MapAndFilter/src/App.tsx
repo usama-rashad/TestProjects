b19037ecc164fa1124af31ac3f56e83f0fdecfda
Usama Rashad <102917763+usama-rashad@users.noreply.github.com>
2022-11-02 19:41:38 +0500

    Update

--- MapAndFilter/src/App.tsx
+++ MapAndFilter/src/App.tsx
@@ -1,18 +1,7 @@
 import "./App.css";
-import Header, { DataParameters } from "./Components/Header";
-import { HeaderContext } from "./Context";
 
 function App() {
-  const headerPars: DataParameters = { pars: ["one", "two", "three"] };
-
-  return (
-    <div>
-      <h4>App</h4>
-      <HeaderContext.Provider value={headerPars}>
-        <Header />
-      </HeaderContext.Provider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- MapAndFilter/src/Context.tsx
+++
@@ -1,5 +0,0 @@
-import { Context, createContext, useContext } from "react";
-
-import { DataParameters } from "./Components/Header";
-
-export const HeaderContext: Context<DataParameters> = createContext({ pars: ["Default value"] });
