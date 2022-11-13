a4d7ca600a2082d79b0301e51140c8108b88b9bb
usama rashad <admin@sssoftware.one>
2022-11-13 17:17:23 +0500

    Update

--- ReactTests_101/src/App.tsx
+++ ReactTests_101/src/App.tsx
@@ -1,16 +1,21 @@
 import "./App.css";
 import Basic from "./Basic";
 
-import {Person} from './Basic'
-import Basic2 from "./Basic2";
+import axios from 'axios'
 
-const peopleData : Person[] = [{id:1,name:"John",age:22},{id:2,name:"Sasha",age:23}]
 
 function App() {
+
+
+
+
+  const postData =()=>{
+    console.log("Love u.")
+  }
+
   return (
     <div className="App">
-        <Basic data={peopleData}/>
-        <Basic2/>
+        <button onClick = {postData}>Post</button>
     </div>
   );
 }
