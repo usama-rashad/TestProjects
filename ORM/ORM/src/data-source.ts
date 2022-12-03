1150cad2b6407c07f4f82ab59499b31e93ff5588
Usama Rashad <usamakr@gmail.com>
2022-12-03 14:01:39 +0500

    Update

--- ORM/ORM/src/data-source.ts
+++ ORM/ORM/src/data-source.ts
@@ -1,17 +1,20 @@
 import "reflect-metadata";
-import {DataSource} from "typeorm";
-import {User} from "./entity/User";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-	type: "mysql",
-	host: "localhost",
-	port: 3307,
-	username: "root",
-	password: "1234",
-	database: "test",
-	synchronize: true,
-	logging: false,
-	entities: [User],
-	migrations: [],
-	subscribers: [],
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "ormuser",
+  password: process.env.ORM_DB_PASSWORD,
+  database: "ormtest",
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+  insecureAuth: false,
 });
