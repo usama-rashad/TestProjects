fcc37b46939e83470e24bfe6abfda2b5ea428a10
Usama Rashad <usamakr@gmail.com>
2022-12-14 13:50:24 +0500

    Update

--- MUI_Themeing/src/Nav/Nav.tsx
+++ MUI_Themeing/src/Nav/Nav.tsx
@@ -2,19 +2,23 @@ import "./Nav.scss";
 
 import React from "react";
 
-function Nav() {
-	return (
-		<div className="nav">
-			<div className="indicator">
-				<div className="line"></div>
-				<div className="stops">
-					<div className="dots"></div>
-					<div className="dots"></div>
-					<div className="dots"></div>
-				</div>
-			</div>
-		</div>
-	);
+export interface IState {
+  state: string;
+}
+
+function Nav(props: IState) {
+  return (
+    <div className={props.state}>
+      <div className="indicator">
+        <div className="line"></div>
+        <div className="stops">
+          <div className="dots"></div>
+          <div className="dots"></div>
+          <div className="dots"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
