d912a31b40e4122fb6220fc8023948322d93678d
usama rashad <admin@sssoftware.one>
2023-01-16 22:12:02 +0500

    Update

--- React_HOC/src/App.tsx
+++ React_HOC/src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import "./App.css";
-import ListComponent from "./components/ListComponent";
+import "./App.scss";
+import LabelWithEdit from "./components/LabelWithEdit";
 
 function App() {
 	return (
-		<div className="app">
+		<div className="App">
 			<h2>React HOC</h2>
-      <ListComponent names={[]}/>
+			<h4>This example demonstrates how to use HOCs to create a simple lable item which can be edited in place</h4>
+			<div className="grid">
+				<LabelWithEdit />
+				<LabelWithEdit />
+				<LabelWithEdit />
+				<LabelWithEdit />
+				<LabelWithEdit />
+				<LabelWithEdit />
+			</div>
 		</div>
 	);
 }
