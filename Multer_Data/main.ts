aeeb2e643546819aa44ca46e82030811e7a6e245
usama rashad <admin@sssoftware.one>
2023-04-29 20:26:09 +0500

    Update

--- Multer_Data/main.ts
+++ Multer_Data/main.ts
@@ -17,14 +17,21 @@ var storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }).single("airlineIconFile");
 
-app.post("/", upload.single("airlineIconFile"), (req: Request, res: Response) => {
-  let { airlineName, airlineHub } = req.body;
-  console.log({ airlineName: airlineName, airlineHub: airlineHub, airlineIconName: req.file?.filename });
-  return res.status(200).json({ server: "OK", response: { 1: airlineName, 2: airlineHub, 3: req.file?.filename } });
+app.post("/", async (req: Request, res: Response) => {
+  upload(req, res, function (error) {
+    if (error instanceof multer.MulterError) {
+      return res.status(400).json({ server: "Error", response: { errorMessage: error } });
+    } else if (error) {
+      return res.status(400).json({ server: "Error", response: { errorMessage: "Unknown error" } });
+    }
+    let { airlineName, airlineHub } = req.body;
+    console.log({ airlineNanme: airlineName, airlineHub: airlineHub, airlineIconName: req.file?.filename });
+    return res.status(200).json({ server: "OK", response: { 1: airlineName, 2: airlineHub, 3: req.file?.filename } });
+  });
 });
 
-app.listen(5000, () => {
+app.listen(4000, () => {
   console.log("Server started.");
 });
